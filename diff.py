b8e1a53647aec41893f0aa91e1d1aeca39e0a414
Gabriele Scalici <gaba41@gmail.com>
2022-05-07 12:38:01 +0200

    refactor and lint

--- diff.py
+++ diff.py
@@ -1,37 +1,43 @@
+"""Calculates diffs between two databases and saves them to a file."""
 import os
 import json
 import subprocess
 
 
 def diff():
+    """The Main Function"""
     files = []
-    for file in os.listdir('./data'):
-        if file.endswith('.db') and not file.startswith('latest'):
+    for file in os.listdir("./data"):
+        if file.endswith(".db") and not file.startswith("latest"):
             files.append(file)
     sort = sorted(files, reverse=True)
     dbs = sort[:2]
     print(dbs)
-    diff = subprocess.Popen(['sqldiff ./data/' + dbs[1] + ' ./data/' + dbs[0]],
-                            shell=True, stdout=subprocess.PIPE, universal_newlines=True).stdout
-    diffList = diff.read().splitlines()
+    differences = subprocess.Popen(
+        [f"sqldiff ./data/{dbs[1]} ./data/{dbs[0]}"],
+        shell=True,
+        stdout=subprocess.PIPE,
+        universal_newlines=True,
+    ).stdout
+    diff_list = differences.read().splitlines()
 
-    lastDB = dbs[0].rpartition('.db')[0]
-    print('Last DB:', lastDB)
+    last_db = dbs[0].rpartition(".db")[0]
+    print("Last DB:", last_db)
 
-    with open('./data/updates.json', "r") as file:
+    with open("./data/updates.json", "r", encoding="utf-8") as file:
         data = json.load(file)
 
-    lastUpdate = data[-1]['date']
-    needsUpdate = lastDB > lastUpdate
+    last_update = data[-1]["date"]
+    needs_update = last_db > last_update
 
-    print('Last update:', lastUpdate)
-    print('New update:', needsUpdate)
+    print("Last update:", last_update)
+    print("New update:", needs_update)
 
-    if needsUpdate:
-        if len(diffList) > 0:
-            print('UPDATE')
-            data.append({'date': lastDB, 'diff': diffList})
-            with open('./data/updates.json', "w") as file:
+    if needs_update:
+        if len(diff_list) > 0:
+            print("UPDATE")
+            data.append({"date": last_db, "diff": diff_list})
+            with open("./data/updates.json", "w", encoding="utf-8") as file:
                 json.dump(data, file)
 
 
--- update.py
+++ update.py
@@ -1,39 +1,41 @@
+"""Update Areas"""
+
 import json
-import pandas as pd
-import requests
 import sqlite3
 import os
-import time
 from datetime import datetime
+import time
+import pandas as pd
+import requests
 
-requests.packages.urllib3.disable_warnings()
 
-portaleURL = "https://www.portaleacque.salute.gov.it/PortaleAcquePubblico/"
-comuniURL = "https://raw.githubusercontent.com/opendatasicilia/comuni-italiani/main/dati/comuni.csv"
-con = sqlite3.connect('./data/latest.db')
-cur = con.cursor()
-req = requests.Session()
+PORTALE_URL = "https://www.portaleacque.salute.gov.it/PortaleAcquePubblico"
+COMUNI = "https://raw.githubusercontent.com/opendatasicilia/comuni-italiani/main/dati/comuni.csv"
+connection = sqlite3.connect("./data/latest.db")
+cursor = connection.cursor()
+request = requests.Session()
 
 headers = {
     "Connection": "keep-alive",
     "sec-ch-ua": '"Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
     "Accept": "application/json",
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
     "sec-ch-ua-mobile": "?0",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36",
     "sec-ch-ua-platform": '"Windows"',
     "Origin": "https://www.portaleacque.salute.gov.it/",
-    'Sec-Fetch-Site': 'same-origin',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Dest': 'empty',
-    'Referer': 'https://www.portaleacque.salute.gov.it/PortaleAcquePubblico/',
-    'Accept-Language': 'en-US,en;q=0.9,it;q=0.8',
-    'Cookie': 'cookies_consent=true'
+    "Sec-Fetch-Site": "same-origin",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Dest": "empty",
+    "Referer": "https://www.portaleacque.salute.gov.it/PortaleAcquePubblico/",
+    "Accept-Language": "en-US,en;q=0.9,it;q=0.8",
+    "Cookie": "cookies_consent=true",
 }
 
-req.headers.update(headers)
+request.headers.update(headers)
 
-cur.execute('''
+cursor.execute(
+    """
 create table if not exists areas (
     CODICE integer primary key not null,
     nome varchar(50) not null,
@@ -56,12 +58,15 @@ create table if not exists areas (
     scheda integer,
     interdizioni text
     );
-''')
-cur.execute('''
+"""
+)
+cursor.execute(
+    """
 create table if not exists version (
     lastUpdate TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
     );
-''')
+"""
+)
 
 codici_istat = {
     "Piemonte": "001272",
@@ -81,122 +86,170 @@ codici_istat = {
     "Basilicata": "076063",
     "Calabria": "079023",
     "Sicilia": "082053",
-    "Sardegna": "092009"
+    "Sardegna": "092009",
 }
 
-comuni = pd.read_csv(comuniURL)
+comuni = pd.read_csv(COMUNI)
 output = []
 
 
-def downJoin(istat):
+def down_join(codice_istat):
+    """Gets features for codice ISTAT"""
     global output
     try:
-        data = req.get(portaleURL+'rest/layer/AB/'+istat, verify=False)
-    except Exception as e:
-        print(e)
+        data = request.get(f"{PORTALE_URL}/rest/layer/AB/{codice_istat}", verify=False)
+    except ConnectionError as error:
+        print(error)
     if data.status_code != 200:
         print(data.status_code)
         return
     data = data.json()
-    for i in range(len(data['features'])):
-        codice = data['features'][i]['properties']['CODICE']
-        geometry = data['features'][i]['geometry']
+    for i in range(len(data["features"])):
+        codice = data["features"][i]["properties"]["CODICE"]
+        geometry = data["features"][i]["geometry"]
         converted = {
             "type": "Feature",
-            "properties": {
-                "CODICE": codice
-            },
-            "geometry": geometry
+            "properties": {"CODICE": codice},
+            "geometry": geometry,
         }
         output.append(converted)
 
 
-def getRegione(provincia):
+def get_regione(provincia):
+    """Get region code from provincia"""
     # print(provincia)
-    if (provincia == 'CI') or (provincia == 'OT') or (provincia == 'VS') or (provincia == 'OG'):
+    if (
+        (provincia == "CI")
+        or (provincia == "OT")
+        or (provincia == "VS")
+        or (provincia == "OG")
+    ):
         return 20
-    elif (provincia == 'NA'):
+    elif provincia == "NA":
         return 15
-    codice = comuni.loc[comuni.sigla == provincia, 'cod_reg'].values[0]
-    return(int(codice))
+    codice = comuni.loc[comuni.sigla == provincia, "cod_reg"].values[0]
+    return int(codice)
 
 
-def insert(featureCollection):
+def insert(feature_collection):
+    """Insert data into DB"""
     # https://www.portaleacque.salute.gov.it/PortaleAcquePubblico/datiArea.do?codiceArea={codice}&tipoArea=undefined&isFuoriNorma=undefined
     timeout = 0
     areas = []
     missing = []
-    cur.execute('BEGIN TRANSACTION;')
-    for index, record in enumerate(featureCollection['features']):
+    cursor.execute("BEGIN TRANSACTION;")
+    for index, record in enumerate(feature_collection["features"]):
         time.sleep(timeout)
-        area = record['properties']['CODICE']
-        data = req.get(portaleURL+'datiArea.do?codiceArea=' +
-                       str(area)+'&tipoArea=undefined&isFuoriNorma=undefined', verify=False)
+        area = record["properties"]["CODICE"]
+        data = request.get(
+            f"{PORTALE_URL}/datiArea.do?codiceArea={str(area)}&tipoArea=undefined&isFuoriNorma=undefined",
+            verify=False,
+        )
         if data.status_code == 200:
             data = data.json()
             converted = {
-                "CODICE": data['areaBalneazioneBean']['codice'],
-                "nome": data['areaBalneazioneBean']['nome'],
-                "comune": data['areaBalneazioneBean']['comune'],
-                "provincia": data['areaBalneazioneBean']['provincia'],
-                "siglaProvincia": data['areaBalneazioneBean']['siglaProvincia'],
-                "regione": getRegione(data['areaBalneazioneBean']['siglaProvincia']),
-                "stato": data['areaBalneazioneBean']['stato'],
-                "limiteEi": data['areaBalneazioneBean']['limiteEi'],
-                "limiteEc": data['areaBalneazioneBean']['limiteEc'],
-                "dataInizioStagioneBalneare": data['areaBalneazioneBean']['dataInizioStagioneBalneare'] if 'dataInizioStagioneBalneare' in data['areaBalneazioneBean'] else '',
-                "dataFineStagioneBalneare": data['areaBalneazioneBean']['dataFineStagioneBalneare'] if 'dataFineStagioneBalneare' in data['areaBalneazioneBean'] else '',
-                "statoDesc": data['areaBalneazioneBean']['statoDesc'],
-                "isFuoriNorma": data['areaBalneazioneBean']['isFuoriNorma'],
-                "geometry": str(record['geometry']['coordinates']),
-                "ultimaAnalisi": str(data['analisi'][0]['dataAnalisi']) if len(data['analisi']) > 0 else str(data['analisiStorico'][0]['dataAnalisi']) if len(data['analisiStorico']) > 0 else None,
-                "valoreEi": str(data['analisi'][0]['valoreEnterococchi']) if len(data['analisi']) > 0 else str(data['analisiStorico'][0]['valoreEnterococchi']) if len(data['analisiStorico']) > 0 else None,
-                "valoreEc": str(data['analisi'][0]['valoreEscherichiaColi']) if len(data['analisi']) > 0 else str(data['analisiStorico'][0]['valoreEscherichiaColi']) if len(data['analisiStorico']) > 0 else None,
-                "flagOltreLimiti": str(data['analisi'][0]['flagOltreLimiti']) if len(data['analisi']) > 0 else str(data['analisiStorico'][0]['flagOltreLimiti']) if len(data['analisiStorico']) > 0 else None,
-                "scheda": data['dettaglioProfiliBean'][0]['codice'] if data['dettaglioProfiliBean'] != None else None,
-                "interdizioni": str(data['interdizioni']) if data['interdizioni'] != None else None
+                "CODICE": data["areaBalneazioneBean"]["codice"],
+                "nome": data["areaBalneazioneBean"]["nome"],
+                "comune": data["areaBalneazioneBean"]["comune"],
+                "provincia": data["areaBalneazioneBean"]["provincia"],
+                "siglaProvincia": data["areaBalneazioneBean"]["siglaProvincia"],
+                "regione": get_regione(data["areaBalneazioneBean"]["siglaProvincia"]),
+                "stato": data["areaBalneazioneBean"]["stato"],
+                "limiteEi": data["areaBalneazioneBean"]["limiteEi"],
+                "limiteEc": data["areaBalneazioneBean"]["limiteEc"],
+                "dataInizioStagioneBalneare": data["areaBalneazioneBean"][
+                    "dataInizioStagioneBalneare"
+                ]
+                if "dataInizioStagioneBalneare" in data["areaBalneazioneBean"]
+                else "",
+                "dataFineStagioneBalneare": data["areaBalneazioneBean"][
+                    "dataFineStagioneBalneare"
+                ]
+                if "dataFineStagioneBalneare" in data["areaBalneazioneBean"]
+                else "",
+                "statoDesc": data["areaBalneazioneBean"]["statoDesc"],
+                "isFuoriNorma": data["areaBalneazioneBean"]["isFuoriNorma"],
+                "geometry": str(record["geometry"]["coordinates"]),
+                "ultimaAnalisi": str(data["analisi"][0]["dataAnalisi"])
+                if len(data["analisi"]) > 0
+                else str(data["analisiStorico"][0]["dataAnalisi"])
+                if len(data["analisiStorico"]) > 0
+                else None,
+                "valoreEi": str(data["analisi"][0]["valoreEnterococchi"])
+                if len(data["analisi"]) > 0
+                else str(data["analisiStorico"][0]["valoreEnterococchi"])
+                if len(data["analisiStorico"]) > 0
+                else None,
+                "valoreEc": str(data["analisi"][0]["valoreEscherichiaColi"])
+                if len(data["analisi"]) > 0
+                else str(data["analisiStorico"][0]["valoreEscherichiaColi"])
+                if len(data["analisiStorico"]) > 0
+                else None,
+                "flagOltreLimiti": str(data["analisi"][0]["flagOltreLimiti"])
+                if len(data["analisi"]) > 0
+                else str(data["analisiStorico"][0]["flagOltreLimiti"])
+                if len(data["analisiStorico"]) > 0
+                else None,
+                "scheda": data["dettaglioProfiliBean"][0]["codice"]
+                if data["dettaglioProfiliBean"] is not None
+                else None,
+                "interdizioni": str(data["interdizioni"])
+                if data["interdizioni"] is not None
+                else None,
             }
             try:
-                cur.execute("INSERT OR REPLACE INTO areas VALUES (:CODICE, :nome, :comune, :provincia, :siglaProvincia, :regione, :stato, :limiteEi, :limiteEc, :dataInizioStagioneBalneare, :dataFineStagioneBalneare, :statoDesc, :geometry, :isFuoriNorma, :ultimaAnalisi, :valoreEi, :valoreEc, :flagOltreLimiti, :scheda, :interdizioni);", converted)
-                print('['+str(index)+'/'+str(len(featureCollection['features']))+']', converted['CODICE'], converted['nome'], converted['comune'],
-                      converted['provincia'], converted['regione'])
+                cursor.execute(
+                    "INSERT OR REPLACE INTO areas VALUES (:CODICE, :nome, :comune, :provincia, :siglaProvincia, :regione, :stato, :limiteEi, :limiteEc, :dataInizioStagioneBalneare, :dataFineStagioneBalneare, :statoDesc, :geometry, :isFuoriNorma, :ultimaAnalisi, :valoreEi, :valoreEc, :flagOltreLimiti, :scheda, :interdizioni);",
+                    converted,
+                )
+                print(
+                    f"[{str(index)}/{str(len(feature_collection['features']))}]",
+                    converted["CODICE"],
+                    converted["nome"],
+                    converted["comune"],
+                    converted["provincia"],
+                    converted["regione"],
+                )
                 areas.append(converted)
-            except Exception as e:
-                print(e)
+            except InterruptedError as error:
+                print(error)
         else:
             missing.append(area)
             continue
     timestamp = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-    cur.execute(
-        "INSERT OR REPLACE INTO version (ROWID, lastUpdate) VALUES (1, ?)", [timestamp])
-    cur.execute('COMMIT;')
-    with open('./data/json/'+timestamp+'.json', 'w+') as f:
-        json.dump(areas, f)
-    os.rename('./data/latest.db', './data/'+timestamp+'.db')
-    print('missing: ', missing)
-    print('Done!')
+    cursor.execute(
+        "INSERT OR REPLACE INTO version (ROWID, lastUpdate) VALUES (1, ?)", [timestamp]
+    )
+    cursor.execute("COMMIT;")
+    with open(f"./data/json/{timestamp}.json", "w+", encoding="utf-8") as file:
+        json.dump(areas, file)
+    os.rename("./data/latest.db", f"./data/{timestamp}.db")
+    print("missing: ", missing)
+    print("Done!")
 
 
 def execute():
-    if not os.path.exists('./data/minified.json'):
+    """The Main Function"""
+    if not os.path.exists("./data/minified.json"):
         global output
-        for istat in codici_istat.values():
-            downJoin(istat)
-        print('Looking for duplicates...')
-        nodups = {i['properties']['CODICE']: i for i in reversed(output)}.values()
+        for codice_istat in codici_istat.values():
+            down_join(codice_istat)
+        print("Looking for duplicates...")
+        nodups = {i["properties"]["CODICE"]: i for i in reversed(output)}.values()
         features = list(nodups)
         output = {"type": "FeatureCollection", "features": features}
-        print('Saving file...')
-        with open('./data/output.json', 'w+') as f:
-            json.dump(output, f)
+        print("Saving file...")
+        with open("./data/output.json", "w+", encoding="utf-8") as file:
+            json.dump(output, file)
         os.system(
-            'mapshaper -i ./data/output.json -snap -simplify weighted 12% keep-shapes -o ./data/minified.json')
-        os.remove('./data/output.json')
-    minified = open('./data/minified.json')
-    featureCollection = json.loads(minified.read())
-    print('Inserting features into db...')
-    insert(featureCollection)
+            "mapshaper -i ./data/output.json -snap -simplify weighted 12% keep-shapes -o ./data/minified.json"
+        )
+        os.remove("./data/output.json")
+    minified = open("./data/minified.json", encoding="utf-8")
+    feature_collection = json.loads(minified.read())
+    print("Inserting features into db...")
+    insert(feature_collection)
 
 
 execute()
-con.close()
+connection.close()
